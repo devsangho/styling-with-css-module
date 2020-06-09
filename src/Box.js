3fe4e5afa37aeb61a14bd30344c2119fa30ada36
devsangho <gaonmail@cau.ac.kr>
2020-06-09 22:42:38 +0900

    checkbox 생성

--- src/Box.js
+++ src/Box.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Box.module.css';
+import styles from './components/Box.module.css';
 
 function Box() {
     return <div className={styles.Box}>{styles.Box}</div>
--- src/CheckBox.js
+++ src/CheckBox.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import styles from './components/CheckBox.module.css';
 
 function CheckBox({ children, checked, ...rest}) {
     return (
-        <div>
+        <div className={styles.checkbox}>
             <label>
                 <input type="checkbox" checked={checked} {...rest} />
-                <div>{checked ? '체크됨' : '체크 안됨'}</div>
+                <div className={styles.icon}>
+                    {checked ? (<MdCheckBox className={styles.checked}/>) : (<MdCheckBoxOutlineBlank />)}
+                </div>
             </label>
-        <span>{children}</span>
+            <span>{children}</span>
         </div>
     );
 }
