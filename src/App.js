c9e91a32d595c9cae56f43fe9ffe0151d5c65caa
devsangho <gaonmail@cau.ac.kr>
2020-06-09 22:12:39 +0900

    CheckBox 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import Box from './Box';
+import CheckBox from './CheckBox';
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = e => {
+    setCheck(e.target.checked);
+  }
   return (
-    <Box></Box>
+    <div>
+      <Box></Box>
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check: </b> {check ? 'true' : 'false'}
+      </p>
+    </div>
+    
   );
 }
 
